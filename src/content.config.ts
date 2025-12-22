1d943e4d6ab08c3a714999383d6a70f3c0568bfb
oizhq-mdr <oilcanguitar@gmail.com>
2025-12-22 23:27:42 +0900

    initial commit

---
+++ src/content.config.ts
@@ -0,0 +1,38 @@
+import { glob } from 'astro/loaders'
+import { defineCollection, z } from 'astro:content'
+
+const posts = defineCollection({
+  // Load Markdown and MDX files in the `src/content/posts/` directory.
+  loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
+  // Type-check frontmatter using a schema
+  schema: () =>
+    z.object({
+      title: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      image: z.string().optional()
+    })
+})
+
+const about = defineCollection({
+  // Load Markdown files in the `src/content/about/` directory.
+  loader: glob({ base: './src/content/about', pattern: '**/*.md' }),
+  // Type-check frontmatter using a schema
+  schema: z.object({})
+})
+
+const publication = defineCollection({
+  // Load Markdown files in the `src/content/publication/` directory.
+  loader: glob({ base: './src/content/publication', pattern: '**/*.md' }),
+  // Type-check frontmatter using a schema
+  schema: z.object({})
+})
+
+const cv = defineCollection({
+  // Load Markdown files in the `src/content/cv/` directory.
+  loader: glob({ base: './src/content/cv', pattern: '**/*.md' }),
+  // Type-check frontmatter using a schema
+  schema: z.object({})
+})
+
+export const collections = { posts, about, publication, cv }
---
+++ src/content/about/about.md
@@ -0,0 +1,13 @@
+---
+title: 'About'
+---
+
+<!--
+This content will be displayed at the top of the index page.
+You can leave this empty if you don’t want to show any content.
+-->
+Hello! I’m an HCI researcher interested in human-centered AI and interactive systems. Starting in 2026, I will pursue my MS at Seoul National University.
+
+> **This Website is Currently Under Construction!**
+
+
